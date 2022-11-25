603409b916599b6cf27748708997af7a9f23e924
aissafatma <43368315+aissafatma@users.noreply.github.com>
2022-11-25 18:26:45 +0100

    Update Pie.jsx

--- cnss/src/pages/Charts/Pie.jsx
+++ cnss/src/pages/Charts/Pie.jsx
@@ -13,7 +13,7 @@ const Pie = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white  rounded-3xl">
     <Header category="Pie" title="Project Cost Breakdown" />
     <div className="w-full">
-    <Indexed/>
+    <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
     </div>
   </div>
   
